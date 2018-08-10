5334ffcc809bde452aea7b83d9983708b6253abc
hjyan <yzy_nchu@163.com>
2018-08-10 16:14:00 +0800

    remove term-server

--- term-server/src/main/java/com/zyyan/ms/term/mqttmodel/base/BaseMqttEntity.java
+++
@@ -1,9 +0,0 @@
-package com.zyyan.ms.term.mqttmodel.base;
-
-import java.io.Serializable;
-
-public class BaseMqttEntity implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-	
-}
